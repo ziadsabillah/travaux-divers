cf2872510a8f4be94d5925a4081e59124d4dfd31
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-05-18 01:43:08 +0100

    initial commit

---
+++ src/components/header/header.data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    path: 'home',
+    label: 'Acceuil',
+  },
+  {
+    path: 'clients',
+    label: 'Clients',
+  },
+  {
+    path: 'feature',
+    label: 'Feature',
+  },
+  {
+    path: 'gallery',
+    label: 'Gallery',
+  },
+  {
+    path: 'pricing',
+    label: 'Pricing',
+  },
+  {
+    path: 'blog',
+    label: 'Blog',
+  },
+];
