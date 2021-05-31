445b91204a67366070dc0e8003bd221419ecd16c
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-05-31 10:50:15 +0100

    removed x transform for gallery bcs it breaks website

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,8 +35,8 @@ export default function IndexPage() {
         </InView>
         <InView threshold={0.5}>
           {({ref, inView}) => (
-            <motion.div style={{ overflowX: 'hidden' }} ref={ref} initial={{ opacity: 0, x: 100  }}
-              animate={inView ? {opacity: 1, x: 0} : {opacity: 0, x: 100}}
+            <motion.div ref={ref} initial={{ opacity: 0 }}
+              animate={inView ? {opacity: 1} : {opacity: 0}}
               transition={{ duration: 0.5 }}>
                 <Gallery />
             </motion.div>
